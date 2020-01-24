0dfc1b2b02e05518a6dafea0e455507066f9182b
Jatin <jatin.shashoo@gmail.com>
2020-01-24 20:01:41 +0530

    Facebook logout integrated

--- src/main/java/com/example/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/com/example/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -3,13 +3,40 @@ package com.example.springsecurityoauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SpringSecurityOauth2Application {
+@RestController
+public class SpringSecurityOauth2Application extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityOauth2Application.class, args);
 	}
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
 
 }
