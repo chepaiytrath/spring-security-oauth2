05746c68c7af472c7a88ef6258a834a80b7b225c
Jatin <jatin.shashoo@gmail.com>
2020-03-02 00:59:24 +0530

    Code refactoring

--- src/main/java/com/example/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/com/example/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -37,6 +37,4 @@ public class SpringSecurityOauth2Application extends WebSecurityConfigurerAdapte
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
-
-
 }
